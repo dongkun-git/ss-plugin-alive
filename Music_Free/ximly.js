3a0b2f69aa98c9a4f332371f2f554116d6f62e08
HSSkyBoy <HSSkyBoy@outlook.com>
2024-06-10 22:38:27 +0800

    更新 ximly.js

--- Music_Free/ximly.js
+++ Music_Free/ximly.js
@@ -1,214 +1,209 @@
-import axios from "axios";
-import dayjs = require("dayjs");
-import CryptoJs = require("crypto-js");
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = require("axios");
+const dayjs = require("dayjs");
 function formatMusicItem(_) {
-  return {
-    id: _.id ?? _.trackId,
-    artist: _.nickname,
-    title: _.title,
-    album: _.albumTitle,
-    duration: _.duration,
-    artwork: _.coverPath?.startsWith("//")
-      ? `https:${_.coverPath}`
-      : _.coverPath,
-  };
+    return {
+        id: _.id ?? _.trackId,
+        artist: _.nickname,
+        title: _.title,
+        album: _.albumTitle,
+        duration: _.duration,
+        artwork: _.coverPath?.startsWith("//")
+            ? `https:${_.coverPath}`
+            : _.coverPath,
+    };
 }
-
 function formatAlbumItem(_) {
-  return {
-    id: _.albumId ?? _.id,
-    artist: _.nickname,
-    title: _.title,
-    artwork: _.coverPath?.startsWith("//")
-      ? `https:${_.coverPath}`
-      : _.coverPath,
-    description: _.intro ?? _.description,
-    date: _.updatedAt ? dayjs(_.updatedAt).format("YYYY-MM-DD") : null,
-  };
+    return {
+        id: _.albumId ?? _.id,
+        artist: _.nickname,
+        title: _.title,
+        artwork: _.coverPath?.startsWith("//")
+            ? `https:${_.coverPath}`
+            : _.coverPath,
+        description: _.intro ?? _.description,
+        date: _.updatedAt ? dayjs(_.updatedAt).format("YYYY-MM-DD") : null,
+    };
 }
-
 function formatArtistItem(_) {
-  return {
-    name: _.nickname,
-    id: _.uid,
-    fans: _.followersCount,
-    description: _.description,
-    avatar: _.logoPic,
-    worksNum: _.tracksCount,
-  };
+    return {
+        name: _.nickname,
+        id: _.uid,
+        fans: _.followersCount,
+        description: _.description,
+        avatar: _.logoPic,
+        worksNum: _.tracksCount,
+    };
 }
-
 function paidAlbumFilter(raw) {
-  return !raw.priceTypes?.length;
+    return !raw.priceTypes?.length;
 }
-
 function paidMusicFilter(raw) {
-  return raw.tag === 0 || raw.isPaid === false || parseFloat(raw.price) === 0;
+    return raw.tag === 0 || raw.isPaid === false || parseFloat(raw.price) === 0;
 }
-
-async function searchBase(query: string, page: number, core: string) {
-  return (
-    await axios.get("https://www.ximalaya.com/revision/search/main", {
-      params: {
-        kw: query,
-        page: page,
-        spellchecker: true,
-        condition: "relation",
-        rows: 20,
-        device: "iPhone",
-        core,
-        paidFilter: true,
-      },
-    })
-  ).data;
+async function searchBase(query, page, core) {
+    return (await axios_1.default.get("https://www.ximalaya.com/revision/search/main", {
+        params: {
+            kw: query,
+            page: page,
+            spellchecker: true,
+            condition: "relation",
+            rows: 20,
+            device: "iPhone",
+            core,
+            paidFilter: true,
+        },
+    })).data;
 }
-
-async function searchMusic(query: string, page: number) {
-  const res = (await searchBase(query, page, "track")).data.track;
-  return {
-    isEnd: page >= res.totalPage,
-    data: res.docs.filter(paidMusicFilter).map(formatMusicItem),
-  };
+async function searchMusic(query, page) {
+    const res = (await searchBase(query, page, "track")).data.track;
+    return {
+        isEnd: page >= res.totalPage,
+        data: res.docs.filter(paidMusicFilter).map(formatMusicItem),
+    };
 }
-
-async function searchAlbum(query: string, page: number) {
-  const res = (await searchBase(query, page, "album")).data.album;
-  return {
-    isEnd: page >= res.totalPage,
-    data: res.docs.filter(paidAlbumFilter).map(formatAlbumItem),
-  };
+async function searchAlbum(query, page) {
+    const res = (await searchBase(query, page, "album")).data.album;
+    return {
+        isEnd: page >= res.totalPage,
+        data: res.docs.filter(paidAlbumFilter).map(formatAlbumItem),
+    };
 }
-
-async function searchArtist(query: string, page: number) {
-  const res = (await searchBase(query, page, "user")).data.user;
-  return {
-    isEnd: page >= res.totalPage,
-    data: res.docs.map(formatArtistItem),
-  };
+async function searchArtist(query, page) {
+    const res = (await searchBase(query, page, "user")).data.user;
+    return {
+        isEnd: page >= res.totalPage,
+        data: res.docs.map(formatArtistItem),
+    };
 }
-
-async function getAlbumInfo(albumItem: IAlbum.IAlbumItem, page: number = 1) {
-  const res = await axios.get(
-    "https://www.ximalaya.com/revision/album/v1/getTracksList",
-    {
-      params: {
-        albumId: albumItem.id,
-        pageNum: page,
-        pageSize: 50,
-      },
+async function getAlbumInfo(albumItem, page = 1) {
+    const res = await axios_1.default.get("https://www.ximalaya.com/revision/album/v1/getTracksList", {
+        params: {
+            albumId: albumItem.id,
+            pageNum: page,
+            pageSize: 50,
+        },
+    });
+    return {
+        isEnd: page * 50 >= res.data.data.trackTotalCount,
+        albumItem: {
+            worksNum: res.data.data.trackTotalCount
+        },
+        musicList: res.data.data.tracks.filter(paidMusicFilter).map((_) => {
+            const r = formatMusicItem(_);
+            r.artwork = albumItem.artwork;
+            r.artist = albumItem.artist;
+            return r;
+        }),
+    };
+}
+async function search(query, page, type) {
+    if (type === "music") {
+        return searchMusic(query, page);
     }
-  );
-  return {
-    isEnd: page * 50 >= res.data.data.trackTotalCount,
-    albumItem: {
-      worksNum: res.data.data.trackTotalCount,
-    },
-    musicList: res.data.data.tracks.filter(paidMusicFilter).map((_) => {
-      const r = formatMusicItem(_);
-      r.artwork = albumItem.artwork;
-      r.artist = albumItem.artist;
-      return r;
-    }),
-  };
-}
-
-async function search(query, page, type: ICommon.SupportMediaType) {
-  if (type === "music") {
-    return searchMusic(query, page);
-  } else if (type === "album") {
-    return searchAlbum(query, page);
-  } else if (type === "artist") {
-    return searchArtist(query, page);
-  }
-}
-
-async function getMediaSource(
-  musicItem: IMusic.IMusicItem,
-  quality: IMusic.IQualityKey
-) {
-  if (quality !== "standard") {
-    return;
-  }
-
-  const info = (
-    await axios.get(
-      `https://www.ximalaya.com/mobile-playpage/track/v3/baseInfo/${Date.now()}?device=www&trackId=${
-        musicItem.id
-      }&trackQualityLevel=1`
-    )
-  ).data;
-
-  const trackInfo = info.trackInfo;
-  const { playUrlList } = trackInfo;
-
-  const encodeText = playUrlList[0].url;
-
-  const url = CryptoJs.AES.decrypt(
-    // @ts-ignore
-    {
-      ciphertext: CryptoJs.enc.Base64url.parse(encodeText),
-    },
-    CryptoJs.enc.Hex.parse("aaad3e4fd540b0f79dca95606e72bf93"),
-    {
-      mode: CryptoJs.mode.ECB,
-      padding: CryptoJs.pad.Pkcs7,
+    else if (type === "album") {
+        return searchAlbum(query, page);
     }
-  ).toString(CryptoJs.enc.Utf8);
-
-  return {
-    url,
-  };
+    else if (type === 'artist') {
+        return searchArtist(query, page);
+    }
 }
-
+async function getMediaSource(musicItem, quality) {
+    if (quality !== "standard") {
+        return;
+    }
+    const data = await axios_1.default.get("https://www.ximalaya.com/revision/play/v1/audio", {
+        params: {
+            id: musicItem.id,
+            ptype: 1,
+        },
+        headers: {
+            referer: `https://www.ximalaya.com/sound/${musicItem.id}`,
+            accept: "*/*",
+            "accept-encoding": "gzip, deflate, br",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61",
+        },
+    });
+    return {
+        url: data.data.data.src,
+    };
+}
 async function getArtistWorks(artistItem, page, type) {
-  if (type === "music") {
-    const res = (
-      await axios.get("https://www.ximalaya.com/revision/user/track", {
-        params: {
-          page,
-          pageSize: 30,
-          uid: artistItem.id,
-        },
-      })
-    ).data.data;
-    return {
-      isEnd: res.page * res.pageSize >= res.totalCount,
-      data: res.trackList.filter(paidMusicFilter).map((_) => ({
-        ...formatMusicItem(_),
-        artist: artistItem.name,
-      })),
-    };
-  } else {
-    const res = (
-      await axios.get("https://www.ximalaya.com/revision/user/pub", {
-        params: {
-          page,
-          pageSize: 30,
-          uid: artistItem.id,
-        },
-      })
-    ).data.data;
-    return {
-      isEnd: res.page * res.pageSize >= res.totalCount,
-      data: res.albumList.filter(paidAlbumFilter).map((_) => ({
-        ...formatAlbumItem(_),
-        artist: artistItem.name,
-      })),
-    };
-  }
+    if (type === "music") {
+        const res = (await axios_1.default.get("https://www.ximalaya.com/revision/user/track", {
+            params: {
+                page,
+                pageSize: 30,
+                uid: artistItem.id,
+            },
+        })).data.data;
+        return {
+            isEnd: res.page * res.pageSize >= res.totalCount,
+            data: res.trackList.filter(paidMusicFilter).map((_) => ({
+                ...formatMusicItem(_),
+                artist: artistItem.name,
+            })),
+        };
+    }
+    else {
+        const res = (await axios_1.default.get("https://www.ximalaya.com/revision/user/pub", {
+            params: {
+                page,
+                pageSize: 30,
+                uid: artistItem.id,
+            },
+        })).data.data;
+        return {
+            isEnd: res.page * res.pageSize >= res.totalCount,
+            data: res.albumList.filter(paidAlbumFilter).map((_) => ({
+                ...formatAlbumItem(_),
+                artist: artistItem.name,
+            })),
+        };
+    }
 }
-
+async function getTopLists() {
+    const resHost = (await axios_1.default.get(`
+    https://m.ximalaya.com/revision/rank/v4/element?rankingId=100006`)).data;
+    const Host = {
+        title: "çƒ­æ¦œ",
+        data: resHost.data.rankList[0].albums.map((_) => {
+            return {
+                id: _.id,
+                title: _.albumTitle,
+                description: _.description,
+                coverImg: `https://imagev2.xmcdn.com/${_.cover}!op_type=3&columns=144&rows=144&magick=webp`
+            };
+        }),
+    };
+    return [Host];
+}
+async function getTopListDetail(topListItem) {
+    const res = (await axios_1.default.get(`https://m.ximalaya.com/m-revision/common/album/queryAlbumTrackRecordsByPage?albumId=${topListItem.id}&page=1&pageSize=100&asc=true`)).data;
+    return {
+        ...topListItem,
+        musicList: res.data.trackDetailInfos.map(_ => {
+            return {
+                id: _.id,
+                albumId: _.trackInfo.albumId,
+                title: _.trackInfo.title,
+            };
+        }),
+    };
+    console.log(res);
+}
 module.exports = {
   platform: "喜马拉雅",
-  author: '猫头猫',
-  version: "0.1.6",
-  supportedSearchType: ["music", "album", "artist"],
-  srcUrl:
-    "https://raw.niuma666bet.buzz/HSSkyBoy/ss-plugin-alive/master/Music_Free/ximly.js",
-  cacheControl: "no-cache",
-  search,
-  getAlbumInfo,
-  getMediaSource,
-  getArtistWorks,
+  author: "HSSkyBoy",
+    version: "0.1.20",
+    order: "0.2.0",
+    srcUrl: "https://raw.gitmirror.com/HSSkyBoy/ss-plugin-alive/master/Music_Free/ximly.js",
+    cacheControl: "no-cache",
+    search,
+    getAlbumInfo,
+    getMediaSource,
+    getArtistWorks,
+    getTopLists,
+    getTopListDetail
 };
